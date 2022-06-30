4921d6267265ab0327313b8384fb7a8a4e001df4
June0619 <jjw5050@hotmail.com>
2022-07-01 02:07:41 +0900

    lecture: javaagent 실습

--- classloader-sample/src/main/java/me/june/MagicianApp.java
+++ classloader-sample/src/main/java/me/june/MagicianApp.java
@@ -1,7 +1,9 @@
 package me.june;
 
 import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.ClassFileLocator;
 import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.pool.TypePool;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,18 +12,6 @@ import static net.bytebuddy.matcher.ElementMatchers.named;
 
 public class MagicianApp {
     public static void main(String[] args) {
-
-//        try {
-//            new ByteBuddy().redefine(MagicianHat.class)
-//                    .method(named("pullOut"))
-//                    .intercept(FixedValue.value("Rabbit!"))
-//                    .make()
-//                    .saveIn(new File("/Users/jjw/workspace/the-java/classloader-sample/target/classes"));
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-
-        //바이트코드를 조작 후 빌드를 새로하는것이 아니면 토끼가 나온다.
         System.out.println(new MagicianHat().pullOut());
     }
 }
