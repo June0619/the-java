fe5b2d57b7e5d88d7072ddc7c788225d24d8c3f3
June0619 <jjw5050@hotmail.com>
2022-07-13 13:21:21 +0900

    lecture: initial commit

---
+++ spring-di-sample/src/test/java/me/june/springdisample/SpringDiSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.june.springdisample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDiSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
